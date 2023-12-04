062a52336fb1300d0bae62e02ada07fb0f4acf98
Guðni Hrafn <gudnihrafn93@gmail.com>
2023-12-04 10:13:47 +0000

    fixed typing

--- _PROJECT/model/destination.py
+++ _PROJECT/model/destination.py
@@ -1,5 +1,7 @@
+from datetime import time
+
 class Destination:
-    def __init__(self, name:str="", airport:str="", flight_time:int=0, flight_distance:float=0, contact_name:str="", contact_num:int=1111111) -> None:
+    def __init__(self, name:str="", airport:str="", flight_time:time=0, flight_distance:float=0, contact_name:str="", contact_num:int=1111111) -> None:
         self.name = name
         self.airport = airport
         self.flight_time = flight_time
