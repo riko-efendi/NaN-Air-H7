ecb36e342435be3aabb43f01443ad3cb995e18dc
Guðni Hrafn <gudnihrafn93@gmail.com>
2023-12-04 10:36:07 +0000

    Added a basic menu, with employee menu

--- _PROJECT/model/flight.py
+++ _PROJECT/model/flight.py
@@ -2,7 +2,7 @@ from datetime import time
 from destination import Destination
 
 class Flight:
-    def __init__(self, depart_country:"Destination" = "", depart_time:time=0, arrival_country:"Destination"="", arrival_time:time=0) -> None:
+    def __init__(self, depart_country:"Destination"=None, depart_time:time=0, arrival_country:"Destination"=None, arrival_time:time=0) -> None:
         self.depart_country = depart_country
         self.depart_time = depart_time
         self.arrival_country = arrival_country
