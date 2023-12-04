620764e954434e99f7c0af9f6b5e03e53b3f86b8
Guðni Hrafn <gudnihrafn93@gmail.com>
2023-12-04 11:22:16 +0000

    merged the cabin crew and pilot to same py file

--- _PROJECT/model/cabin_crew.py
+++
@@ -1,5 +0,0 @@
-from employee import Employee
-
-class CabinCrew(Employee):
-    def __init__(self):
-        super().__init__()
--- _PROJECT/model/employee.py
+++ _PROJECT/model/employee.py
@@ -1,7 +1,16 @@
 class Employee:
-    def __init__(self, kennitala, address, phone_number, email):
+    def __init__(self, name, kennitala, address, phone_number, email):
         self.name = name
         self.kennitala = kennitala
         self.address = address
         self.phone_number = phone_number
         self.email = email
+
+class Pilot(Employee):
+    def __init__(self, rank="Co-Pilot"):
+        super().__init__()
+        self.rank = rank
+
+class CabinCrew(Employee):
+    def __init__(self):
+        super().__init__()
