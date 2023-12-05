ab66eb4233a6cadc8a292fd83f87de930d037892
Guðni Hrafn <gudnihrafn93@gmail.com>
2023-12-05 11:47:20 +0000

    Update logic_wrapper.py

--- _PROJECT/logic/logic_wrapper.py
+++ _PROJECT/logic/logic_wrapper.py
@@ -1,12 +1,13 @@
 from logic.employee_logic import EmployeeLogic
 from logic.destination_logic import DestinationLogic
-
+from data.data_wrapper import DataWrapper
 """
 Employee base class. Here we give the employee all its variables, and behaviours.
 """
 
 class LogicWrapper:
     def __init__(self) -> None:
+        self.data_wrapper = DataWrapper()
         self.employee_logic = EmployeeLogic(self.data_wrapper)
         self.destination_logic = DestinationLogic(self.data_wrapper)
 
