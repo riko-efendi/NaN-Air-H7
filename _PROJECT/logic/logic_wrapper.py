f27a3af7f6e020e92feb89521dd63b1e3bb19103
Guðni Hrafn <gudnihrafn93@gmail.com>
2023-12-05 11:23:30 +0000

    Comments on the models

--- _PROJECT/logic/logic_wrapper.py
+++ _PROJECT/logic/logic_wrapper.py
@@ -1,13 +1,15 @@
 from logic.employee_logic import EmployeeLogic
-
 from data.data_wrapper import DataWrapper
+
+"""
+Employee base class. Here we give the employee all its variables, and behaviours.
+"""
 
 class LogicWrapper:
     def __init__(self) -> None:
 
         self.data_wrapper = DataWrapper()
 
-        self.data_wrapper = DataWrapper()
         self.employee_logic = EmployeeLogic(self.data_wrapper)
 
     def get_all_employees(self):
--- _PROJECT/model/destination.py
+++ _PROJECT/model/destination.py
@@ -1,7 +1,11 @@
 from datetime import time
 
+"""
+Destination base class. Here we give the destination all its variables, and behaviours.
+"""
+
 class Destination:
-    def __init__(self, name:str="", airport:str="", flight_time:time=0, flight_distance:float=0, contact_name:str="", contact_num:int=1111111) -> None:
+    def __init__(self, id:int=0, name:str="", numeric_id:int=1, airport:str="", flight_time:time=0, flight_distance:float=0, contact_name:str="", contact_num:int=1111111) -> None:
         self.name = name
         self.airport = airport
         self.flight_time = flight_time
