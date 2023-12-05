5f57f2da888ada9fbdef87119d86aa074281675f
Riko Efendi <riko.efend@gmail.com>
2023-12-05 11:21:55 +0000

    List all destinations works

--- _PROJECT/logic/logic_wrapper.py
+++ _PROJECT/logic/logic_wrapper.py
@@ -1,4 +1,5 @@
 from logic.employee_logic import EmployeeLogic
+from logic.destination_logic import DestinationLogic
 
 from data.data_wrapper import DataWrapper
 
@@ -6,10 +7,12 @@ class LogicWrapper:
     def __init__(self) -> None:
 
         self.data_wrapper = DataWrapper()
-
-        self.data_wrapper = DataWrapper()
         self.employee_logic = EmployeeLogic(self.data_wrapper)
+        self.destination_logic = DestinationLogic(self.data_wrapper)
 
     def get_all_employees(self):
         return self.employee_logic.get_all_employees()
+
+    def get_all_destinations(self):
+        return self.destination_logic.get_all_destinations()
 
