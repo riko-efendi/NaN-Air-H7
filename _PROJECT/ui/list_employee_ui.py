e4b721fd1c12d4946091736060bb3bd5cff2c459
Guðni Hrafn <gudnihrafn93@gmail.com>
2023-12-06 10:24:31 +0000

    Updated the employee ui

--- _PROJECT/ui/list_employee_ui.py
+++ _PROJECT/ui/list_employee_ui.py
@@ -1,12 +1,14 @@
+from utils.ui_utils import UIUtils
 
 class ListEmployeeUI:
     def __init__(self, wrapper) -> None:
         self.logic_wrapper = wrapper
+        self.ui_utils = UIUtils()
 
     def menu_output(self):
         header = f"[View Employee Options]"
 
-        print()
+
         print(header)
         print()
 
@@ -25,14 +27,16 @@ class ListEmployeeUI:
 
             if user_input == "b":
                 break
-            
+
             elif user_input == "1":
                 employees = self.logic_wrapper.get_all_employees()
-                print()
-                print("[ALL EMPLOYEES]")
-                print()
+                self.ui_utils.clear_screen()
+                print("[ALL EMPLOYEES]\n")
+
                 for index, employee in enumerate(employees):
                     print(f"{index+1:>2}.{' name: ':^2}{employee.name:<}, {employee.role}\n      {'kt: ' + employee.print_kennitala}")
+
+                input("\nPress [ENTER] to exit: ")
             elif user_input == "2":
                 kennitala_input = input("Enter Employee' Kennitala: ")
                 print(self.logic_wrapper.get_employee_by_nid(kennitala_input))
