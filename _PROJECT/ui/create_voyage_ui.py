20be812e46f1bb982c3165c2614756ef4eeb135e
MannyBoyy <manwin8@gmail.com>
2023-12-06 15:27:40 +0000

    Fuckin around

--- _PROJECT/ui/create_voyage_ui.py
+++ _PROJECT/ui/create_voyage_ui.py
@@ -1,13 +1,24 @@
 from utils.utils import UIUtils
+from model.voyage import Voyage
+
+
 
 class CreateVoyageUI:
-    def __init__(self) -> None:
-        pass
-
+    def __init__(self, wrapper) -> None:
+        self.logic_wapper = wrapper
+        self.ui_utils = UIUtils
     def menu_output():
         header = f"[CREATING VOYAGES]"
-
+        """We start by giving the user 2 options of either creating or editing the voyage"""
         print()
         print(header)
+        print()
         print(f"1. Create new voyage")
         print(f"2. Edit voyage")
+
+    
+    def input_promt(self):
+        while True: 
+            self.menu_output()
+            user_input =  input("\nEnter your choice: ").lower()
+            # Clears screen ==> self.ui_utils.clear_screen() m  
