bee2a3a198cb3a8acbda5135a8fef887aaf209b4
Guðni Hrafn <gudnihrafn93@gmail.com>
2023-12-08 08:42:33 +0000

    flight model class

--- _PROJECT/model/flight.py
+++ _PROJECT/model/flight.py
@@ -1,4 +1,4 @@
-from datetime import time
+from datetime import datetime
 
 
 
@@ -13,6 +13,7 @@ class Flight:
         arr_date, arr_time = arrival.split(" ")
         self.flight_nr = flight_nr
         self.aircraft_id = aircraft_id
+        self.flight_dur = self.calculate_flight_duration(depart_time, arr_time)
 
         # DEPARTURE
         self.dep_from = dep_from
@@ -26,7 +27,17 @@ class Flight:
         self.arr_time = arr_time
         self.arr_dest = None
 
+
+
+    
     def __str__(self) -> str:
         return_str = f"Flight number: {self.flight_nr}.\nDeparting from:\t{self.dep_from} \t[{self.depart_date} {self.depart_time}]\nArriving at:\t{self.arr_at} \t[{self.arr_date} {self.arr_time}]"
-
+        return_str += f"\nThe duration of the flight is {self.flight_dur}\n"
         return return_str
+    
+    def calculate_flight_duration(self, time_1, time_2):
+        time_format = "%H:%M:%S"
+        time_1 = datetime.strptime(time_1, time_format)
+        time_2 = datetime.strptime(time_2, time_format)
+
+        return time_2 - time_1
