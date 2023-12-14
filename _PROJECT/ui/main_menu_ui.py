e31f3cdf861c4c54a7b32fe2a5ec1c1f16786a99
Riko Efendi <riko.efend@gmail.com>
2023-12-14 13:03:29 +0000

    main menu front end

--- _PROJECT/ui/main_menu_ui.py
+++ _PROJECT/ui/main_menu_ui.py
@@ -3,6 +3,7 @@ from ui.employee_ui import EmployeeUI
 from ui.voyage_ui import VoyageUI
 from utils.ui_utils import UIUtils
 
+DASH_AMOUNT = 46
 
 class MainMenuUI:
     def __init__(self) -> None:
@@ -13,11 +14,15 @@ class MainMenuUI:
     def menu_output(self) -> None:
         """Prints out the options for the Main Menu UI"""
 
+        header = "[Main Menu]"
         self.ui_utils.clear_screen()
-        print(f"[MAIN]\n")
-        print(f"1. Employees")
-        print(f"2. Voyage")
-        print(f"\n[Q]uit")
+        print(header + "-" * (DASH_AMOUNT - len(header)) + "\n")
+        print("\n" * 4)
+        print(f"\t1. Employees\n")
+        print(f"\t2. Voyage\n")
+        print("\n" * 4)
+        print(f"\t\t\t\t\t[Q]uit")
+        print("-" * DASH_AMOUNT)
 
     def input_prompt(self) -> None:
         """Takes in an input from user, and jumpst to a specific UI/function based on that input."""
